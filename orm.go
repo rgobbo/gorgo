66f535a102f63b077fffe6a9233725fe08ed216f
rgobbo <rgobbo@hotmail.com>
2018-07-25 19:01:50 -0300

    changes to orm

--- orm.go
+++ orm.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-type DB struct {
+type ORM struct {
 	dialectDB Dialect
-	showSQL  bool
+	showSQL   bool
 }
 
 type FuncMap map[string]interface{}
 
-func NewDB(config ConfigDB) (*DB, error) {
+func NewOrm(config ConfigDB) (*ORM, error) {
 	dbtype := config.Type
 	if dbtype == "" {
 		return nil, fmt.Errorf("DB_TYPE not defined.")
@@ -26,14 +26,14 @@ func NewDB(config ConfigDB) (*DB, error) {
 		if err != nil {
 			return nil, err
 		}
-		return &DB{dialectDB: dialect}, nil
+		return &ORM{dialectDB: dialect}, nil
 	case "localdb":
 		dialect := &LocalDialect{}
 		err := dialect.InitDB(config)
 		if err != nil {
 			return nil, err
 		}
-		return &DB{dialectDB: dialect}, nil
+		return &ORM{dialectDB: dialect}, nil
 	default:
 		return nil, fmt.Errorf("[WARNING] dbtype not found", nil)
 	}
@@ -41,76 +41,74 @@ func NewDB(config ConfigDB) (*DB, error) {
 	return nil, nil
 }
 
-func (d *DB)NewSession() *Session {
-	session := &Session{db: d}
+func (d *ORM) NewSession() *Session {
+	session := &Session{orm: d}
 	session.Init()
 	return session
 }
 
-func (d *DB) Table(tbl string) *Session  {
+func (d *ORM) Table(tbl string) *Session {
 	session := d.NewSession()
 	session.tableName = tbl
 	return session
 }
 
-func (d *DB) Limit(i int) *Session  {
+func (d *ORM) Limit(i int) *Session {
 	session := d.NewSession()
 	session.limit = i
 	return session
 }
 
-func (d *DB) Offset(i int) *Session  {
+func (d *ORM) Offset(i int) *Session {
 	session := d.NewSession()
 	session.offset = i
 	return session
 }
 
-func (d *DB) Where(query string, params ...interface{}) *Session {
+func (d *ORM) Where(query string, params ...interface{}) *Session {
 	session := d.NewSession()
 	session.where = query
 	session.params = params
 	return session
 }
 
-// Query a raw sql and return records as []map[string][]byte
-func (d *DB) Get() ([]JSONDoc, error) {
+// Get Query a raw sql and return records as []map[string][]byte
+func (d *ORM) Get() ([]JSONDoc, error) {
 	session := d.NewSession()
 	return session.Get()
 }
 
-func (d *DB) GetByID( id string) (JSONDoc, error) {
+func (d *ORM) GetByID(id string) (JSONDoc, error) {
 	session := d.NewSession()
 	return session.GetByID(id)
 
 }
 
-func (d *DB) Insert(data JSONDoc) (JSONDoc, error) {
+func (d *ORM) Insert(data JSONDoc) (JSONDoc, error) {
 	session := d.NewSession()
 	return session.Insert(data)
 }
 
-func (d *DB) Update( data JSONDoc) error {
+func (d *ORM) Update(data JSONDoc) error {
 	session := d.NewSession()
 	return session.Update(data)
 }
 
-func (d *DB) DeleteByID( id string) error {
+func (d *ORM) DeleteByID(id string) error {
 	session := d.NewSession()
 	return session.DeleteByID(id)
 }
 
-func (d *DB) DeleteByWhere( ) error {
+func (d *ORM) DeleteByWhere() error {
 	session := d.NewSession()
 	return session.DeleteByWhere()
 }
 
-func (d *DB) Count( ) (int, error) {
+func (d *ORM) Count() (int, error) {
 	session := d.NewSession()
 	return session.Count()
 }
 
-func (d *DB) Close() error {
+func (d *ORM) Close() error {
 	return d.dialectDB.CloseDB()
 }
-
-
--- session.go
+++ session.go
@@ -4,16 +4,16 @@ import "fmt"
 
 type Session struct {
 	tableName string
-	limit int
-	offset int
-	where string
-	order string
-	params []interface{}
-	colums string
-	pk  string
-	join     string
-	groupBy  string
-	db *DB
+	limit     int
+	offset    int
+	where     string
+	order     string
+	params    []interface{}
+	colums    string
+	pk        string
+	join      string
+	groupBy   string
+	orm       *ORM
 }
 
 func (s *Session) Init() {
@@ -28,50 +28,49 @@ func (s *Session) Where(query string, params ...interface{}) *Session {
 	return s
 }
 
-
-func (s *Session) Limit(i int) *Session  {
+func (s *Session) Limit(i int) *Session {
 	s.limit = i
 	return s
 }
 
-func (s *Session) Offset(i int) *Session  {
+func (s *Session) Offset(i int) *Session {
 	s.offset = i
 	return s
 }
 
-func (s *Session) Get()([]JSONDoc, error) {
+func (s *Session) Get() ([]JSONDoc, error) {
 	if s.tableName == "" {
 		return []JSONDoc{}, fmt.Errorf("need to set a tablename")
 	}
 	if s.where != "" {
-		s.db.dialectDB.GetManyByQuery(s.tableName,s.where, s.params...)
+		s.orm.dialectDB.GetManyByQuery(s.tableName, s.where, s.params...)
 	}
-	return s.db.dialectDB.GetAll(s.tableName, s.offset, s.limit, s.order)
+	return s.orm.dialectDB.GetAll(s.tableName, s.offset, s.limit, s.order)
 }
 
 func (s *Session) GetByID(id string) (JSONDoc, error) {
 	if s.tableName == "" {
 		return JSONDoc{}, fmt.Errorf("need to set a tablename")
 	}
-	return s.db.dialectDB.GetById(s.tableName, id)
+	return s.orm.dialectDB.GetById(s.tableName, id)
 }
 
-func (s *Session) Insert( data JSONDoc) (JSONDoc, error) {
+func (s *Session) Insert(data JSONDoc) (JSONDoc, error) {
 	if s.tableName == "" {
 		return JSONDoc{}, fmt.Errorf("need to set a tablename")
 	}
-	return s.db.dialectDB.Create(s.tableName, data)
+	return s.orm.dialectDB.Create(s.tableName, data)
 }
 
-func (s *Session) Update( data JSONDoc) error {
+func (s *Session) Update(data JSONDoc) error {
 	if s.tableName == "" {
 		return fmt.Errorf("need to set a tablename")
 	}
-	return s.db.dialectDB.Update(s.tableName, data)
+	return s.orm.dialectDB.Update(s.tableName, data)
 }
 
 func (s *Session) DeleteByID(id string) error {
-	return s.db.dialectDB.Delete(s.tableName, id)
+	return s.orm.dialectDB.Delete(s.tableName, id)
 
 }
 
@@ -79,16 +78,15 @@ func (s *Session) DeleteByWhere() error {
 	if s.where == "" {
 		return fmt.Errorf("need where clause")
 	}
-	return s.db.dialectDB.Delete(s.tableName, s.where)
+	return s.orm.dialectDB.Delete(s.tableName, s.where)
 
 }
 
-
 func (s *Session) Count() (int, error) {
 	if s.tableName == "" {
-		return 0,fmt.Errorf("need to set a tablename")
+		return 0, fmt.Errorf("need to set a tablename")
 	}
-	i, err := s.db.dialectDB.Count(s.tableName)
+	i, err := s.orm.dialectDB.Count(s.tableName)
 	if err != nil {
 		return 0, err
 	}
